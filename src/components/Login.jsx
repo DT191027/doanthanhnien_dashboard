df71cf012fd7ba7f803248cb12c3865541fe0881
DT191027 <dtnxts2026@gmail.com>
2026-09-11 21:37:19 +0700

    update_login_placeholder_exact

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -86,7 +86,7 @@ export default function Login({ onLoginSuccess }) {
                   <input 
                     type="text" 
                     className="form-control bg-light border-start-0 ps-0" 
-                    placeholder="ap...@xuanthoison.gov.vn hoặc mn...@xuanthoison.gov.vn"
+                    placeholder="ap...@xuanthoison.gov.vn hoặc mn..@xuanthoison.gov.vn"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
